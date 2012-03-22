3a852143dee1dc719d1b68ecff4440115067adf0
Tino <Tino@Tino-PC>
2012-03-22 19:45:16 +0100

    opponent now always plays away from the player
    Change-Id: I9470fee1ec0a75374787c36ba7ed476ebabc430d

---
+++ src/de/redlion/badminton/Opponent.java
@@ -0,0 +1,70 @@
+package de.redlion.badminton;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
+
+public class Opponent {
+
+	public enum STATE {
+		IDLE, UP, DOWN, LEFT, RIGHT, DOWNLEFT, UPLEFT, DOWNRIGHT, UPRIGHT;
+	}
+
+	public Vector3 direction = new Vector3(0, 1, 0);
+	public Vector3 center = new Vector3(-0.10882504f,-2.8946533f,-0.5f);
+	public Vector3 position = center.cpy();
+	public Vector3 velocity = new Vector3(0,0,0); //for jumps
+	public STATE state = STATE.IDLE;
+	public boolean jump = false;
+	
+	public Birdie bird;
+
+	public Opponent(Birdie birdie) {
+		bird = birdie;
+	}
+
+	public void update(Vector3 playerposition) {
+		
+		Vector3 distance = position.cpy().sub(bird.position.cpy());
+		Vector3 tmp = playerposition.cpy();
+		tmp.x = - playerposition.x;
+		tmp.y = 10 - playerposition.y;
+		direction = position.cpy().sub(tmp).mul(-1);
+		direction.nor();
+		
+		if(bird.state == Birdie.STATE.HIT || bird.position.cpy().y < 0) {
+			if(position.y < 0) {
+				position.x = position.x - distance.x * Gdx.graphics.getDeltaTime() / 4;
+				position.y = position.y - distance.y * Gdx.graphics.getDeltaTime() / 4;
+				position.z = position.z - distance.z * Gdx.graphics.getDeltaTime() / 4;
+			}
+			else
+				position.y = -0.01f;
+		}
+		else if(bird.state == Birdie.STATE.HITBYOPPONENT){
+			distance = position.cpy().sub(center.cpy());
+			
+			position.x = position.x - distance.x * Gdx.graphics.getDeltaTime() / 2;
+			position.y = position.y - distance.y * Gdx.graphics.getDeltaTime() / 2;
+			position.z = position.z - distance.z * Gdx.graphics.getDeltaTime() / 2;
+			
+		}
+		else {
+			//SCOOOOOREEEE!!
+		}
+			
+
+	}
+	
+	public void jump()
+	{
+//		jump=true;
+//		velocity = direction;
+//		velocity.x *= 6.0f/5;
+//		velocity.y *= 6.0f/2;
+//		velocity.z = -6.0f;
+	}
+
+}

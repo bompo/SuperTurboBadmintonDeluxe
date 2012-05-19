8a618be651463b08cb02acf91ae9112636c018ff
pavethem <tino.helmig@gmx.de>
2012-05-19 21:43:17 +0200

    added experimental birdie behavior

--- src/de/redlion/badminton/Birdie.java
+++ src/de/redlion/badminton/Birdie.java
@@ -1,6 +1,9 @@
 package de.redlion.badminton;
 
+import java.util.ArrayList;
+
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.math.CatmullRomSpline;
 import com.badlogic.gdx.math.Vector3;
 
 import de.redlion.badminton.Player.AIMING;
@@ -17,6 +20,8 @@ public class Birdie {
 	public Vector3 toPosition = new Vector3(-2, 7, -0.5f);
 	public Vector3 velocity = new Vector3(0, 0, 0);
 	
+	CatmullRomSpline spline = new CatmullRomSpline();
+	ArrayList<Vector3> path = new ArrayList<Vector3>();
 
 	float acceleration = 6.0f;
 
@@ -33,12 +38,17 @@ public class Birdie {
 			velocity.mul((float) Math.pow(0.97f, Gdx.graphics.getDeltaTime() * 30.f));			
 			
 			//TODO: http://www.real-world-physics-problems.com/physics-of-volleyball.html			
-			currentPosition.x = fromPosition.x * velocity.x  + (1 - toPosition.x);
-			currentPosition.y = fromPosition.y * velocity.y  + (1 - toPosition.y);
-			currentPosition.z = fromPosition.z * velocity.z  + (1 - toPosition.z);
+//			currentPosition.x = fromPosition.x * velocity.x  + (1 - toPosition.x);
+//			currentPosition.y = fromPosition.y * velocity.y  + (1 - toPosition.y);
+//			currentPosition.z = fromPosition.z * velocity.z  + (1 - toPosition.z);
 			
-			if (currentPosition.z < 0) {
-				currentPosition.z = currentPosition.z + (Gdx.graphics.getDeltaTime() * 2.f);
+			
+			if(path.size() > 0) {
+				currentPosition = path.get(0);
+				path.remove(0);
+			
+//			if (currentPosition.z < 0) {
+//				currentPosition.z = currentPosition.z + (Gdx.graphics.getDeltaTime() * 2.f);
 			} else {
 				state = STATE.NONHIT;
 				score();
@@ -73,6 +83,7 @@ public class Birdie {
 	}
 
 	public void reset() {
+		path = new ArrayList<Vector3>();
 		if (currentPosition.y < 0) {
 			currentPosition = Resources.getInstance().player.position.cpy()
 					.add(-0.5f, 0, 0);
@@ -99,6 +110,13 @@ public class Birdie {
 			toPosition.set(2, 2, 0);
 		else if (player.aiming == Player.AIMING.RIGHT)
 			toPosition.set(-2, 2, 0);
+		else
+			toPosition.set(-2, 2, 0);
+		
+		spline.add(fromPosition);
+		spline.add(new Vector3(2,0,-2));
+		spline.add(toPosition);
+		path = (ArrayList<Vector3>) spline.getPath(50);
 	}
 
 	public String toString() {

5bc59d0e3ae4dec53eef3ef5f122981292e5513a
pavethem <tino.helmig@gmx.de>
2012-06-20 20:00:10 +0200

    less expensive trajectory trail

--- src/de/redlion/badminton/GameScreen.java
+++ src/de/redlion/badminton/GameScreen.java
@@ -241,32 +241,35 @@ public class GameScreen extends DefaultScreen implements InputProcessor {
 		
 		// render birdie trajectory
 		if(birdie.trajectoryPath.size > 0) {
-			for(Vector3 vex : birdie.trajectoryPath) {
-				tmp.idt();
-				model.idt();
-		
-				tmp.setToRotation(Vector3.X, 90);
-				model.mul(tmp);
-		
-				Vector3 tmpV = birdie.currentPosition;
+			int asd = birdie.trajectoryPath.indexOf(birdie.currentPosition, false);
+			for(int i = asd; i > asd - 20; i--) {
+				if(i>0) {
+					tmp.idt();
+					model.idt();
+			
+					tmp.setToRotation(Vector3.X, 90);
+					model.mul(tmp);
+			
+					//Vector3 tmpV = birdie.currentPosition;
+					
+					//if(birdie.trajectoryPath.indexOf(birdie.currentPosition, false) - birdie.trajectoryPath.indexOf(vex, false) < 21)
+					Vector3	tmpV = birdie.trajectoryPath.get(i);
+					
+					tmp.setToTranslation(tmpV.x, tmpV.y,
+							tmpV.z - 0.1f);
+					model.mul(tmp);
+					
+					tmp.setToScaling(0.1f, 0.1f, 0.1f);
+					model.mul(tmp);
+			
+					diffuseShader.setUniformMatrix("MMatrix", model);
+					diffuseShader.setUniformi("uSampler", 0);
+			
+					modelPlaneTex.bind(0);
+					modelPlaneObj.render(diffuseShader);
+				}
 				
-				if(birdie.trajectoryPath.indexOf(birdie.currentPosition, false) - birdie.trajectoryPath.indexOf(vex, false) < 21)
-					tmpV = vex;
-				
-				tmp.setToTranslation(tmpV.x, tmpV.y,
-						tmpV.z - 0.1f);
-				model.mul(tmp);
-				
-				tmp.setToScaling(0.1f, 0.1f, 0.1f);
-				model.mul(tmp);
-		
-				diffuseShader.setUniformMatrix("MMatrix", model);
-				diffuseShader.setUniformi("uSampler", 0);
-		
-				modelPlaneTex.bind(0);
-				modelPlaneObj.render(diffuseShader);
-				
-		}
+			}
 		}
 
 		// render shadow
